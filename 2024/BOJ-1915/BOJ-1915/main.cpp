3734ecf94a4e8eba3d8a472a37b6ae7b9b37db72
권영재 <dwdjjj@naver.com>
2024-11-22 15:28:29 +0900

    24.11.22 update BOJ solved in Mac

---
+++ 2024/BOJ-1915/BOJ-1915/main.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int n, m;
+    cin >> n >> m;
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            
+        }
+    }
+    
+    return 0;
+}
--- 2024/bfsPractice/bfsPractice/main.cpp
+++ 2024/bfsPractice/bfsPractice/main.cpp
@@ -1,58 +1,69 @@
-//
-//  main.cpp
-//  bfsPractice
-//
-//  Created by 권영재 on 2024/03/05.
-//
-
 #include <iostream>
-#include<vector>
-#include<queue>
+#include <vector>
+#include <queue>
 using namespace std;
 int dy[4] = {0, 1, 0, -1};
 int dx[4] = {1, 0, -1, 0};
-vector<vector<int>> d;
-// bool isInBoard(int y, int x) {
-    
-// }
+int n, m;
+vector<vector<int>> board;
+vector<vector<int>> ans;
 
-void bfs(vector<vector<int>> maps) {
+bool isInBoard(int y, int x) {
+     return 0 <= y && 0 <= x && y < n && x < m;
+ }
+
+void bfs(int y, int x) {
     queue<pair<int, int>> q;
-    q.push({0,0});
-    pair<int ,int> cur;
-    d.resize(maps.size(), vector<int>(maps[0].size(), 0));
-    int ny, nx;
+    q.push({y, x});
+    ans[y][x] = 0;
+    
     while(!q.empty()) {
-        cur = q.front();
+        pair<int, int> cur = q.front();
+        int curY = cur.first;
+        int curX = cur.second;
         q.pop();
+        
         for(int i=0; i<4; i++) {
-            ny = cur.first + dy[i]; nx = cur.second + dx[i];
-            if(ny>= 0 && nx >= 0 && ny < maps.size() && nx < maps[0].size()) {
-                if(maps[ny][nx] == 1 && d[ny][nx] == 0) {
-                    q.push({ny, nx});
-                    d[ny][nx] = d[cur.first][cur.second] + 1;
-                    cout << d[ny][nx] << "\n";
-                }
-            }
+            int nextY = curY + dy[i];
+            int nextX = curX + dx[i];
+            
+            if(!isInBoard(nextY, nextX) || ans[nextY][nextX] >= 0) continue;
+            if(board[nextY][nextX] == 0) continue;
+            ans[nextY][nextX] = ans[curY][curX] + 1;
+            q.push({nextY, nextX});
         }
     }
+
+    return;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    vector<vector<int>> maps;
-    maps.resize(5, vector<int>(5, 0));
-    for(int i=0; i<5; i++) {
-        for(int j=0; j<5; j++) {
-            cin >> maps[i][j];
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    ans.resize(n, vector<int>(m, -1));
+    board.resize(n, vector<int>(m, 0));
+    int y = 0, x = 0;
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> board[i][j];
+            if(board[i][j] == 2) {
+                y = i; x = j;
+            }
         }
     }
+    bfs(y, x);
     
-    int answer = 0;
-    bfs(maps);
-    answer = d[maps.size()-1][maps[0].size()-1];
-    cout << answer;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            (board[i][j] == 0) ?
+            cout << board[i][j] << " ":
+            cout << ans[i][j] << " ";
+        }
+        cout << "\n";
+    }
     
     return 0;
 }
